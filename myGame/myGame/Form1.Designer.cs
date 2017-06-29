4be9e97b271574d5f8461498b32c9fa89014a8d6
Mert Çeken <mertceken12@gmail.com>
2017-06-29 22:21:21 +0300

    Mario Properties
    Basit bir karakter yapısının sağ, sol ve zıplama yönlendirmeleri ile hareketlendirilmesi. (Mario Mantığı)

---
+++ myGame/myGame/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace myGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.screen = new System.Windows.Forms.Panel();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.block = new System.Windows.Forms.PictureBox();
+            this.screen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.block)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // screen
+            // 
+            this.screen.Controls.Add(this.block);
+            this.screen.Controls.Add(this.player);
+            this.screen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.screen.Location = new System.Drawing.Point(0, 0);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(284, 261);
+            this.screen.TabIndex = 0;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Black;
+            this.player.Location = new System.Drawing.Point(23, 38);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(25, 25);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // block
+            // 
+            this.block.BackColor = System.Drawing.Color.Gray;
+            this.block.Location = new System.Drawing.Point(142, 127);
+            this.block.Name = "block";
+            this.block.Size = new System.Drawing.Size(100, 50);
+            this.block.TabIndex = 1;
+            this.block.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.screen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.screen.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.block)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel screen;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox block;
+    }
+}
+
---
+++ myGame/myGame/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myGame
+{
+    public partial class Form1 : Form
+    {
+        bool right;
+        bool left;
+        bool jump;
+        int G = 30;
+        int force;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(player.Right > block.Left && player.Left < block.Right - player.Width/ 2 && player.Bottom > block.Top)
+            {
+                right = false;
+            }
+
+            if(player.Left < block.Right && player.Right > block.Left + player.Width/ 2 && player.Bottom > block.Top)
+                {
+                left = false;
+                }
+
+
+            if (right == true) { player.Left += 5; }
+            if (left == true) { player.Left -= 5; }
+
+            if (jump != true)
+            {
+                player.Top -= force;
+                force -= -1;
+            }
+            if ( player.Top + player.Height >= screen.Height)
+            {
+                player.Top = screen.Height - player.Height;
+                jump = false;
+            }
+            else
+            {
+                player.Top += 5;
+
+            }
+
+          if(player.Left + player.Width - 1 > block.Left && player.Left + player.Width + 5 < block.Left + block.Width + player.Width && player.Top + player.Height >= player.Top)
+            {
+                player.Top = screen.Height - block.Height - player.Height;
+                force = 0;
+                jump = false;
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) { right = true; }
+            if (e.KeyCode == Keys.Left)  { left = true; }
+            if (e.KeyCode == Keys.Escape { this.Close(); }
+
+            if (jump != true)
+            {
+                if(e.KeyCode == Keys.Space)
+                {
+                    jump = true;
+                    force = 6;
+
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) { right = false; }
+            if (e.KeyCode == Keys.Left) { left = false; }
+        }
+    }
+}
