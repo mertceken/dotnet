455484ab682e924c3007ff353979a782d359ac6f
Mert Çeken <mertceken12@gmail.com>
2017-07-02 22:49:21 +0300

    MVC Login Page
    Mvc, Login Page formu

---
+++ LoginPageMVC/LoginPageMVC/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginPageMVC.ViewModels;
+
+
+namespace LoginPageMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            AccountViewModel avm = new AccountViewModel();
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Login(AccountViewModel avm)
+        {
+            if(avm.Account.Username.Equals("abc") && avm.Account.Username.Equals("123"))
+            {
+                Session["username"] = avm.Account.Username;
+                return View("Welcome");
+
+            }
+            else
+            {
+                ViewBag.Error = "Account's Invalid";
+               return View();
+            }
+            
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Remove("username");
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ LoginPageMVC/LoginPageMVC/ViewModels/AccountViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LoginPageMVC.Models;
+namespace LoginPageMVC.ViewModels
+{
+    public class AccountViewModel
+    {
+        public Account Account
+        {
+            get;
+            set;
+
+        }
+    }
+}
