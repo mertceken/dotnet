6bf7b7118dab2c950dcebed8155bf83e5da53a19
Mert Çeken <mertceken12@gmail.com>
2019-01-27 22:46:01 +0300

    Donate Automation
    Donate Automation Application with Entity Framework. Using db first.

---
+++ 190126_ef_db_first_tutorial/190126_bireysel_proje/Forms/frmLogin.cs
@@ -0,0 +1,57 @@
+﻿using _190126_bireysel_proje.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _190126_bireysel_proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUserName.Text=="admin" && txtPassword.Text=="1234" )
+            {
+            
+                frmDashboard dsh = new frmDashboard();
+                dsh.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Login is failed. Please Try Again..");
+            }
+            
+        }
+
+        private void txtUserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUserName_Click(object sender, EventArgs e)
+        {
+            txtUserName.Clear();
+        }
+
+        private void txtPassword_Click(object sender, EventArgs e)
+        {
+            txtPassword.Clear();
+        }
+    }
+}
