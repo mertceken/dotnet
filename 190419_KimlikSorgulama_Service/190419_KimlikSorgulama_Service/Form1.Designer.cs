71e6430d5df3c49dbeb1848c876b88f15a04080c
Mert Çeken <mertceken12@gmail.com>
2019-04-19 21:31:37 +0300

    TCKN Sorgulama
    Web Service yardımıyla TC kimlik numarası sorgulaması yapan basit bir form tasarımı.
    
    --
    
    A simple windows form design for Turkish ID Number questioning with using Web Service

---
+++ 190419_KimlikSorgulama_Service/190419_KimlikSorgulama_Service/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace _190419_KimlikSorgulama_Service
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maskedTextBoxDogumyili = new System.Windows.Forms.MaskedTextBox();
+            this.textBoxAdi = new System.Windows.Forms.TextBox();
+            this.textBoxSoyadi = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.maskedTextBoxTC = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(257, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SORGULA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ADI";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "SOYADI";
+            // 
+            // maskedTextBoxDogumyili
+            // 
+            this.maskedTextBoxDogumyili.Location = new System.Drawing.Point(83, 180);
+            this.maskedTextBoxDogumyili.Mask = "0000";
+            this.maskedTextBoxDogumyili.Name = "maskedTextBoxDogumyili";
+            this.maskedTextBoxDogumyili.Size = new System.Drawing.Size(56, 20);
+            this.maskedTextBoxDogumyili.TabIndex = 4;
+            // 
+            // textBoxAdi
+            // 
+            this.textBoxAdi.Location = new System.Drawing.Point(83, 78);
+            this.textBoxAdi.Name = "textBoxAdi";
+            this.textBoxAdi.Size = new System.Drawing.Size(153, 20);
+            this.textBoxAdi.TabIndex = 5;
+            // 
+            // textBoxSoyadi
+            // 
+            this.textBoxSoyadi.Location = new System.Drawing.Point(83, 134);
+            this.textBoxSoyadi.Name = "textBoxSoyadi";
+            this.textBoxSoyadi.Size = new System.Drawing.Size(153, 20);
+            this.textBoxSoyadi.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "DOĞUM YILI";
+            // 
+            // maskedTextBoxTC
+            // 
+            this.maskedTextBoxTC.Location = new System.Drawing.Point(85, 28);
+            this.maskedTextBoxTC.Mask = "00000000000";
+            this.maskedTextBoxTC.Name = "maskedTextBoxTC";
+            this.maskedTextBoxTC.Size = new System.Drawing.Size(151, 20);
+            this.maskedTextBoxTC.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 356);
+            this.Controls.Add(this.textBoxSoyadi);
+            this.Controls.Add(this.textBoxAdi);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.maskedTextBoxTC);
+            this.Controls.Add(this.maskedTextBoxDogumyili);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxDogumyili;
+        private System.Windows.Forms.TextBox textBoxAdi;
+        private System.Windows.Forms.TextBox textBoxSoyadi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxTC;
+    }
+}
+
---
+++ 190419_KimlikSorgulama_Service/190419_KimlikSorgulama_Service/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using _190419_KimlikSorgulama_Service.TCKimlikNoServiceReference;
+
+namespace _190419_KimlikSorgulama_Service
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //using _190419_KimlikSorgulama_Service.TCKimlikNoServiceReference; eklenmeli
+      
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            KPSPublicSoapClient sonuc = new KPSPublicSoapClient();
+
+            
+            long tc =Convert.ToInt64( maskedTextBoxTC.Text);
+            string adi = textBoxAdi.Text.ToUpper();
+            string soyadi = textBoxSoyadi.Text.ToUpper();
+            int dogumyili =Convert.ToInt32(maskedTextBoxDogumyili.Text);
+            bool gelen = sonuc.TCKimlikNoDogrula(tc, adi, soyadi, dogumyili);
+
+            if (gelen)
+            {
+
+                MessageBox.Show("***********Nerelisin??************\nİçinden mi??");
+            }
+            else
+            {
+                MessageBox.Show("Bu ülkeden değilsin??");
+            }
+        }
+    }
+}
