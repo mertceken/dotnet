e771e8465335fd0795a58c038b051e9b68ffdb1d
Mert Çeken <mertceken12@gmail.com>
2019-05-18 15:08:07 +0300

    .NET Core First App
    My first .NET Core console application

---
+++ 180519_Core_Intro/180509_Core_Intro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _180509_Core_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen isminizi giriniz");
+
+            var isim = Console.ReadLine();
+
+            Console.WriteLine($"Merhaba {isim}");
+
+            Console.ReadKey();
+        }
+    }
+}
