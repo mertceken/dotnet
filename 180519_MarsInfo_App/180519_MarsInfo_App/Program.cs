3a3b7b8f4154f9de3a0aa335968506ce5431098b
Mert Çeken <mertceken12@gmail.com>
2019-05-18 15:22:05 +0300

    .NET Core Mars Distance Information app
    A simple .NET Core console application

---
+++ 180519_MarsInfo_App/180519_MarsInfo_App/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+
+namespace _180519_MarsInfo_App
+{
+    public class Mars
+    {
+        public const int SpeedOfLight = 300000;
+        public readonly int distancetomars = 0;
+
+        public Mars()
+        {
+            distancetomars = Convert.ToInt32(ConfigurationManager.AppSettings["DistanceToMars"]);
+        }
+
+        public int GetInfoTravelTime()
+        {
+            return distancetomars / SpeedOfLight;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mars mars = new Mars();
+
+            Console.WriteLine($"Information travel time us approximately {mars.GetInfoTravelTime()} seconds from Mars to Earth");
+        }
+    }
+}
