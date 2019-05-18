c016d00526a39cf7ed3e41f235a4889a5f307766
Mert Çeken <mertceken12@gmail.com>
2019-05-18 15:11:40 +0300

    .NET Core If-Else Statement Example
    .Net core if-else example with console application

---
+++ 180519_If_Else_ex/180519_If_Else_ex/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _180519_If_Else_ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMoneyInSavingAccount = true;
+
+            if (isMoneyInSavingAccount)
+            {
+                Console.WriteLine("I have money");
+            }
+            else
+            {
+                Console.WriteLine("I have no money");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
