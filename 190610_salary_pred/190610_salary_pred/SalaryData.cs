7af1b61c58cc273a3cfecd75271a8f7c0835df85
Mert Çeken <mertceken12@gmail.com>
2019-06-10 17:02:24 +0300

    Salary Prediction with ML.NET Model
    Bir veri grubu içinden maaş tahmini hesaplayan bir ML.NET modeli
    
    ---
    A ML.NET model which is calculating salary prediction from dataset

---
+++ 190610_salary_pred/190610_salary_pred/SalaryData.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.ML.Runtime.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _190610_salary_pred
+{
+    public class SalaryData
+    {
+        [Column("0")]
+        public float YILLAR;
+
+        [Column("1", name: "Label")]
+        public float MAAS;
+    }
+}
---
+++ 190610_salary_pred/190610_salary_pred/SalaryPrediction.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.ML.Runtime.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _190610_salary_pred
+{
+    public class SalaryPrediction
+    {
+        [ColumnName("Score")]
+        public float PredictedSalary;
+
+        // Maaş tahmininin başarısı
+    }
+}
